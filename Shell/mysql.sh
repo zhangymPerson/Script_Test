31cc1b0d908b77453b885ae1229f72d034089f53
zym-aliyun <546666.happy@163.com>
2019-03-09 11:02:33 +0800

    code(mysql)读取mysql密码文件命令

--- Shell/mysql.sh
+++ Shell/mysql.sh
@@ -6,22 +6,7 @@
 ########################################
 echo "Hello Word"
 
-
-function getPw {
-    local pwFile=$1
-    local key=$2
-    echo "密码文件:"$pwFile",关键字:"$key
-    return grep $key $pwFile|awk '{print $2}'
-
-}
-
-
-
 pwFile=passwd.txt
 key=one
-
-echo getPw $pwFile $key
-
 mysql_password=$(grep one ${pwFile}|awk '{print $2}')
-
 echo "mysqlPw = "$mysql_password
