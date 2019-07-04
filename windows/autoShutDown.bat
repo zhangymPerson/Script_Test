7e30669e1757a5c023817ac00242a8d5583b153d
zhangym-aliyun <zhangyanmingjiayou@163.com>
2019-07-04 10:09:19 +0800

    alter autoshutdown.bat

--- windows/autoShutDown.bat
+++ windows/autoShutDown.bat
@@ -2,7 +2,9 @@
 :: dir c:\*.* > a.txt
 ::dir c:\*.* > b.txt
 ::dir c:\*.* >> c.txt
-::自动关机命令
-echo 半小时后即将自动关机
+::??????????
+echo delete shutdown
+shutdown -a
+echo shutdown after 1800
 shutdown -s -t 1800
 ::pause
