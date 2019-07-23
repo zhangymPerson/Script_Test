91ce44b23270479b2aae47ff5dd429dc0d477c37
zhangym <zhangyanmingjiayou@163.com>
2019-07-23 16:36:15 +0800

    alter windows shell

--- windows/test.bat
+++ windows/test.bat
@@ -1,8 +1,7 @@
-::代表注释
-::测试 windows脚本需要使用GBK / ANSI编码，否则中文乱码
+:: note use ::
 @echo off
-echo "你好TEST!"
-::命令执行状态码 与shell中 $? 类似
+echo "TEST!"
+::no use chinese
 echo %errorlevel%
-::执行完后暂停 会出现请按任意键继续
+::执stop
 pause
